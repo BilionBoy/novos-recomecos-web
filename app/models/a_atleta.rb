e5eb97da424949cc17054299df0c4d28197a992a
Dhiogoandre <dhiogoandre26@gmail.com>
2025-07-26 14:16:49 -0400

    wip: cadastro do atleta

--- app/models/a_atleta.rb
+++ app/models/a_atleta.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
 class AAtleta < ApplicationRecord
-  # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
+  # Relacionamentos
+  belongs_to :g_estado
+  belongs_to :g_cidade
+  belongs_to :a_unidade_treinamento
+  belongs_to :a_sexo
+
+  # Validações 
+  validates :nome,                   presence: true
+  validates :cpf,                    presence: true
+  validates :data_nascimento,        presence: true
+  validates :contato1,               presence: true
+  validates :contato2,               presence: true
+  validates :nacionalidade,          presence: true
+  validates :nome_pai,               presence: true
+  validates :nome_mae,               presence: true
+  validates :responsavel_nome,       presence: true
+  validates :responsavel_cpf,        presence: true
+  validates :responsavel_rg,         presence: true
+  validates :responsavel_orientacao, presence: true
+  validates :portador_necessidade,   presence: true
+  validates :descricao_necessidade,  presence: true
+  validates :cep,                    presence: true
+  validates :rua,                    presence: true
+  validates :bairro,                 presence: true
+  validates :numero,                 presence: true
+  validates :complemento,            presence: true
+
+
 end
--- db/migrate/20250726174610_create_a_atletas.rb
+++ db/migrate/20250726174610_create_a_atletas.rb
@@ -2,38 +2,40 @@
 
 class CreateAAtletas < ActiveRecord::Migration[7.2]
   def up
-    create_table :a_atletas do |t|
-      t.string     :nome
-      t.string     :cpf
-      t.date       :data_nascimento
-      t.string     :contato1
-      t.string     :contato2
-      t.string     :nacionalidade
-      t.string     :nome_pai
-      t.string     :nome_mae
-      t.string     :responsavel_nome
-      t.string     :responsavel_cpf
-      t.string     :responsavel_rg
-      t.text       :responsavel_orientacao
-      t.boolean    :portador_necessidade
-      t.text       :descricao_necessidade
-      t.string     :cep
-      t.string     :rua
-      t.string     :bairro
-      t.string     :numero
-      t.string     :complemento
-      t.references :g_estado,              foreign_key: true
-      t.references :g_cidade,              foreign_key: true
-      t.references :a_unidade_treinamento, foreign_key: true
-      t.references :a_sexo,                foreign_key: true
-      t.string     :created_by
-      t.string     :updated_by
-      t.datetime   :deleted_at
-      t.timestamps
+    unless table_exists?(:a_atletas)
+     create_table :a_atletas do |t|
+       t.string     :nome
+       t.string     :cpf
+       t.date       :data_nascimento
+       t.string     :contato1
+       t.string     :contato2
+       t.string     :nacionalidade
+       t.string     :nome_pai
+       t.string     :nome_mae
+       t.string     :responsavel_nome
+       t.string     :responsavel_cpf
+       t.string     :responsavel_rg
+       t.text       :responsavel_orientacao
+       t.boolean    :portador_necessidade
+       t.text       :descricao_necessidade
+       t.string     :cep
+       t.string     :rua
+       t.string     :bairro
+       t.string     :numero
+       t.string     :complemento
+       t.references :g_estado,              foreign_key: true
+       t.references :g_cidade,              foreign_key: true
+       t.references :a_unidade_treinamento, foreign_key: true
+       t.references :a_sexo,                foreign_key: true
+       t.string     :created_by
+       t.string     :updated_by
+       t.datetime   :deleted_at
+       t.timestamps
+     end
     end
   end
 
   def down
-    drop_table :a_atletas
+    drop_table :a_atletas if table_exists?(:a_atletas)
   end
 end
